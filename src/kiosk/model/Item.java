9d2274d49005893e483625a1d47deb7b130f8cf4
gloz0315 <qudtn0315@naver.com>
2024-01-07 23:08:31 +0900

    메뉴 클래스와 상품 클래스에 대한 기능 생성

--- src/kiosk/model/Item.java
+++ src/kiosk/model/Item.java
@@ -1,4 +1,14 @@
 package kiosk.model;
 
-public class Item {
+public class Item extends Menu{
+    private final double price;
+
+    public Item(String name, String description, double price) {
+        super(name, description);
+        this.price = price;
+    }
+
+    public double itemPrice() {
+        return price;
+    }
 }
