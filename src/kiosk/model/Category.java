2336e0e75c5dc80f1398524e6741846d1e261e8d
gloz0315 <qudtn0315@naver.com>
2024-01-08 15:09:00 +0900

    메인화면의 카테고리별 목록에 대한 Enum클래스 생성 및 기능추가

--- src/kiosk/model/Category.java
+++ src/kiosk/model/Category.java
@@ -1,4 +1,43 @@
 package kiosk.model;
 
 public enum Category {
+    BURGERTYPE(1,"Burgers","앵거스 비프 통살을 다져만든 버거"),
+    ICECREAMTYPE(2,"Ice Cream","매장에서 신선하게 만드는 아이스크림"),
+    DRINKTYPE(3,"Drinks","매장에서 직접 만드는 음료"),
+    FRIEDTYPE(4,"Fried","뉴욕 브루클린 브루어리에서 만든 감자튀김"),
+    ORDERTYPE(5,"Order","장바구니를 확인 후 주문합니다."),
+    CANCELTYPE(6,"Cancel","진행중인 주문을 취소합니다."),
+    SALESTYPE(7,"Sales","총 매출 정보를 보여줍니다.");
+
+    private final int number;
+    private final String type;
+    private final String description;
+
+    Category(int number, String type, String description) {
+        this.number = number;
+        this.type = type;
+        this.description = description;
+    }
+
+    public static Category getCategoryInfo(int number) {
+        int count = 1;
+        for(Category category : Category.values()) {
+            if(number == category.typeNumber())
+                return category;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    public int typeNumber() {
+        return number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String typeDescription() {
+        return description;
+    }
 }
