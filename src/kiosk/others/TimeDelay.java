a5b0d82d3dd708e2f61ed87266ff3eb866cf69b0
gloz0315 <qudtn0315@naver.com>
2024-01-08 15:09:58 +0900

    시간 지연 클래스 생성 및 기능 추가

--- src/kiosk/others/TimeDelay.java
+++ src/kiosk/others/TimeDelay.java
@@ -1,4 +1,19 @@
 package kiosk.others;
 
 public class TimeDelay {
+    private static void timeDelay() throws InterruptedException {
+        for(int i = 3; i > 0; i--) {
+            System.out.printf("[ %d초 뒤에 화면이 이동합니다. ]\n", i);
+            Thread.sleep(1000);
+        }
+        System.out.println();
+    }
+
+    public static void delay() {
+        try {
+            TimeDelay.timeDelay();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
