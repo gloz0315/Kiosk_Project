dbc942d4be3ab733b24e7ea82de67398b3298d72
gloz0315 <qudtn0315@naver.com>
2024-01-08 15:09:29 +0900

    입력값이 숫자에 대한 정보인지에 대한 기능 생성

--- src/kiosk/view/InputView.java
+++ src/kiosk/view/InputView.java
@@ -1,4 +1,25 @@
 package kiosk.view;
 
+import kiosk.console.Console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputView {
+
+    public static int matchPattern(String number) {
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(number);
+
+        if(matcher.matches()) {
+            return Integer.parseInt(number);
+        }
+
+        throw new IllegalArgumentException();
+    }
+    public static String inputView() {
+        System.out.print("숫자를 입력하세요 > ");
+        return Console.readLine();
+    }
 }
